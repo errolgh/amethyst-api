8e56b2f13340ef569254a2ce941e195bc92efbc0
Andrew Katz <andrew@greenbits.com>
2018-10-15 07:04:00 -0700

    Add old route

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,12 +5,25 @@ class ReportsController < ApplicationController
   end
 
   def create
-    report = Report.create(status: Report::STATUS[:queued])
+    report = create_report
     BuildReportJob.perform_later(report.id)
     render_report(report)
   end
 
+  # NOTE: Old synchronous version for backwards compatibility.
+  # Clients should be updated to use create and show.
+  def old_show
+    report = create_report
+    BuildReportJob.perform_now(report.id)
+    report.reload
+    render_report(report)
+  end
+
   private
+
+  def create_report
+    Report.create(status: Report::STATUS[:queued])
+  end
 
   def render_report(report)
     render json: { report: report.as_json }
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ require 'resque/server'
 Rails.application.routes.draw do
   post 'report', to: 'reports#create'
   get 'report/:id', to: 'reports#show'
+  get 'report', to: 'reports#old_show'
 
   mount Resque::Server, at: '/resque'
 end
